6049777378becf84e3102353f1e0b2731ecfb9d7
Davide <69810644+ItzDavi@users.noreply.github.com>
2022-10-17 12:47:12 +0200

    Lessons initial commit
    Minor bug fixes, 1 new icon,  2 new colors

--- app/src/main/java/com/davidemolo/learnkotlin/topics/TopicViewModel.kt
+++ app/src/main/java/com/davidemolo/learnkotlin/topics/TopicViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 
 class TopicViewModel(
     val topicTitle: String,
+    //TODO prima o poi mettere topicTime float cosi da fare 0.1 ore, 1.3 ore ecc
     val topicTime: Int,
     val topicLessons: Int,
     val topicDifficulty: Int,
